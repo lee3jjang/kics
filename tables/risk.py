dff43ba10684f4442c1a413df87d170287f69226
SangJin <lee3jjang@gmail.com>
2021-03-15 21:07:58 +0900

    테이블 생성

--- tables/risk.py
+++ tables/risk.py
@@ -1,16 +1,57 @@
 # 최종수정일자 : 2021.03.15 
 
-############# 테이블 목록 ##############
+############### 테이블 목록 ################
+# (신규) KICS_BOZ_CD_RISK_NL (KAKD0008LM)
+# (신규) KICS_BOZ_GRP_RISK_NL (XXXX0000LM)
 # (수정) KICS_CNTR_RISK_NL (KAKD0002LM)
 # KICS_TOT_RISK_NL (KAJC0011LM)
-#######################################
+###########################################
+
+################ 일반손해보험위험액 산출순서 ################
+# KICS국가그룹, 보장단위별 보험가격/준비금 위험액 (KAKD0008LM)
+# KICS국가그룹 및 보장단위별 위험액 (KAKD0008LM)
+# KICS국가그룹 및 보장그룹별 위험액 (XXXX0000LM)
+# KICS국가그룹별 위험액 (KAKD0002LM)
+# 보험가격준비금위험액 위험액 (KAJC0011LM)
+# 일반손해보험 위험액 (KAJC0011LM)
+###########################################################
 
 import sqlite3
 
 conn = sqlite3.connect('kics.db')
 cur = conn.cursor()
 
-# (수정) KICS_CNTR_RISK_NL(KICS국가그룹별 요구자본)
+# (수정) KICS_BOZ_CD_RISK_NL(KICS국가그룹 및 보장단위별 위험액)
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS KICS_BOZ_CD_RISK_NL (
+        BASE_DATE TEXT,                 /* 기준일자 */
+        KICS_CNTR_CATG_CD TEXT,         /* KICS국가그룹코드 */
+        BOZ_CD TEXT,                    /* 보종코드 */
+        RETAIN_LAPS_PRM NUMERIC,        /* 보험료보유익스포져 */
+        RETAIN_RSV_LIAB NUMERIC,        /* 준비금보유익스포져 */
+        PREM_RISK_AMT NUMERIC,          /* 보험가격위험액 */
+        RSV_RISK_AMT NUMERIC,           /* 준비금위험액 */
+        BOZ_CD_RISK NUMERIC,            /* 보장단위별위험액 */
+        LAST_MODIFIED_BY TEXT,
+        LAST_UPDATE_DATE TEXT,
+        PRIMARY KEY (BASE_DATE, KICS_CNTR_CATG_CD, BOZ_CD)
+    )
+""")
+
+# (신규) KICS_BOZ_GRP_RISK_NL(KICS국가그룹 및 보장그룹별 위험액)
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS KICS_BOZ_GRP_RISK_NL (
+        BASE_DATE TEXT,                 /* 기준일자 */
+        KICS_CNTR_CATG_CD TEXT,         /* KICS국가그룹코드 */
+        BOZ_CATG_CD TEXT,               /* 보장그룹코드 */
+        BOZ_GRP_RISK NUMERIC,           /* 보장그룹별위험액 */
+        LAST_MODIFIED_BY TEXT,
+        LAST_UPDATE_DATE TEXT,
+        PRIMARY KEY (BASE_DATE, KICS_CNTR_CATG_CD, BOZ_CATG_CD)
+    )
+""")
+
+# (수정) KICS_CNTR_RISK_NL(KICS국가그룹별 위험액)
 cur.execute("""
     CREATE TABLE IF NOT EXISTS KICS_CNTR_RISK_NL (
         BASE_DATE TEXT,                 /* 기준일자 */
