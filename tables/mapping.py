28566dadd730b2dc06dcdeee0ce7327153911cb0
SangJin <lee3jjang@gmail.com>
2021-03-15 20:44:52 +0900

    테이블 생성

--- tables/mapping.py
+++ tables/mapping.py
@@ -1,8 +1,8 @@
 # 최종수정일자 : 2021.03.15 
 
-############# 테이블 목록 #############
+############# 테이블 목록 ##############
 # KICS_CNTR_GRP_NL
-######################################
+#######################################
 
 import sqlite3
 
@@ -12,10 +12,10 @@ cur = conn.cursor()
 # KICS_CNTR_GRP_NL(국가그룹 -> KICS국가그룹 맵핑)
 cur.execute("""
     CREATE TABLE IF NOT EXISTS KICS_CNTR_GRP_NL (
-        CNTR_CATG_CD TEXT,
-        CNTR_CATG_NM TEXT,
-        KICS_CNTR_CATG_CD TEXT,
-        KICS_CNTR_CATG_NM TEXT,
+        CNTR_CATG_CD TEXT,              /* 국가그룹코드 */
+        CNTR_CATG_NM TEXT,              /* 국가그룹명 */
+        KICS_CNTR_CATG_CD TEXT,         /* KICS국가그룹코드 */
+        KICS_CNTR_CATG_NM TEXT,         /* KICS국가그룹명 */
         LAST_MODIFIED_BY TEXT,
         LAST_UPDATE_DATE TEXT,
         PRIMARY KEY (CNTR_CATG_CD)
@@ -25,8 +25,8 @@ cur.execute("""
 # (수정) KICS_BOZ_GRP_MAP_NL(보장단위 -> 보장그룹 맵핑)
 cur.execute("""
     CREATE TABLE IF NOT EXISTS KICS_BOZ_GRP_MAP_NL (
-        BOZ_CD TEXT,
-        BOZ_CATG_CD TEXT,
+        BOZ_CD TEXT,                    /* 보종코드 */
+        BOZ_CATG_CD TEXT,               /* 보장그룹코드 */
         LAST_MODIFIED_BY TEXT,
         LAST_UPDATE_DATE TEXT,
         PRIMARY KEY (BOZ_CD)
--- tables/risk.py
+++ tables/risk.py
@@ -1,8 +1,9 @@
 # 최종수정일자 : 2021.03.15 
 
-############# 테이블 목록 #############
+############# 테이블 목록 ##############
 # (수정) KICS_CNTR_RISK_NL (KAKD0002LM)
-######################################
+# KICS_TOT_RISK_NL (KAJC0011LM)
+#######################################
 
 import sqlite3
 
@@ -12,11 +13,28 @@ cur = conn.cursor()
 # (수정) KICS_CNTR_RISK_NL(KICS국가그룹별 요구자본)
 cur.execute("""
     CREATE TABLE IF NOT EXISTS KICS_CNTR_RISK_NL (
-        BASE_DATE TEXT,
-        KICS_CNTR_CATG_CD TEXT,
-        CNTR_RISK NUMERIC,
+        BASE_DATE TEXT,                 /* 기준일자 */
+        KICS_CNTR_CATG_CD TEXT,         /* KICS국가그룹코드 */
+        CNTR_RISK NUMERIC,              /* KICS국가그룹별위험액 */
         LAST_MODIFIED_BY TEXT,
         LAST_UPDATE_DATE TEXT,
         PRIMARY KEY (BASE_DATE, KICS_CNTR_CATG_CD)
+    )
+""")
+
+# KICS_TOT_RISK_NL(일반손해보험위험액)
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS KICS_TOT_RISK_NL (
+        BASE_DATE TEXT,                 /* 기준일자 */
+        CAT_CAL_CD TEXT,                /* 대재해위험액산출기준(1: 원수, 2: 보유)
+        CNTR_RISK NUMERIC,              /* KICS국가그룹별위험액(합계) */
+        KICS_PRM_RSV_RISK NUMERIC,      /* 보험가격준비금위험액 */
+        KICS_IND_CAT_RISK NUMERIC,      /* 대재해위험액(단순합계) */
+        KICS_CAT_RISK NUMERIC,          /* 대재해위험액(상관관계반영) */
+        DVS_EPCT NUMMERIC,              /* 분산효과 */
+        KICS_NON_LIKE_RISK NUMERIC,     /* 일반손해보험위험액 */
+        LAST_MODIFIED_BY TEXT,
+        LAST_UPDATE_DATE TEXT,
+        PRIMARY KEY (BASE_DATE, CAT_CAL_CD)
     )
 """)
