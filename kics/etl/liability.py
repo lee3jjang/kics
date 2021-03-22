da70965d4d0aee00d4252b95394a7d74289d6bf6
SangJin <lee3jjang@gmail.com>
2021-03-22 18:07:08 +0900

    부채산출 프로세스 추가

---
+++ kics/etl/liability.py
@@ -0,0 +1,49 @@
+import os
+import math
+import sqlite3
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+
+####################### 프로그램 목록 #######################
+# KAJC0003LM: 현금흐름비중 산출
+# KAJC0004LM: 할인요소 산출
+# KAJC0002LM: 가정 산출
+# 적용 가정 산출
+# KAJC0001LM: 일반 보험료부채 익스포져 산출
+# KAJC0009LM: 자동차 보험료부채 익스포져 산출
+# 일반 준비금부채 익스포져 산출
+# 자동차 준비금부채 익스포져 산출
+# 보험료부채 산출
+# 준비금부채 산출
+# KAJC0013LM: 재보험자산 손실조정 산출
+############################################################
+
+def KAKD0003LM(base_date: str) -> pd.DataFrame:
+    # 최종수정일자: 2021.03.22
+    # TODO: 로직구현
+    pass
+
+def KAJC0004LM(base_date: str) -> pd.DataFrame:
+    # 최종수정일자: 2021.03.22
+    # TODO: 로직구현
+    pass
+
+def KAJC0002LM(base_date: str) -> pd.DataFrame:
+    # 최종수정일자: 2021.03.22
+    # TODO: 로직구현
+    pass
+
+def KAJC0001LM(base_date: str) -> pd.DataFrame:
+    # 최종수정일자: 2021.03.22
+    # TODO: 로직구현
+    pass
+
+def KAJC0009LM(base_date: str) -> pd.DataFrame:
+    # 최종수정일자: 2021.03.22
+    # TODO: 로직구현
+    pass
+
+def KAJC0013LM(base_date: str) -> pd.DataFrame:
+    pass
